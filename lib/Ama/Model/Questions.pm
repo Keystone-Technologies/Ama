f647c99e91ac046096aff528a8f7fbd59bfc9664
Stefan Adams <stefan@borgia.com>
2015-06-02 09:40:32 -0500

    improve questions index by showing votes, answers, etc

--- lib/Ama/Model/Questions.pm
+++ lib/Ama/Model/Questions.pm
@@ -9,9 +9,39 @@ sub add {
   return $self->pg->db->query($sql, $question->{question}, $username)->hash->{id};
 }
 
-sub all { shift->pg->db->query(q[select questions.id,question,SUM(CASE WHEN vote = 'up' THEN 1 ELSE 0 END) as votes_up, SUM(CASE WHEN vote = 'down' THEN 1 ELSE 0 END) as votes_down from questions left join votes on questions.id=votes.entry_id and votes.entry_type='questions' group by questions.id])->hashes->to_array }
-sub answered { shift->pg->db->query(q[select questions.id,question,SUM(CASE WHEN vote = 'up' THEN 1 ELSE 0 END) as votes_up, SUM(CASE WHEN vote = 'down' THEN 1 ELSE 0 END) as votes_down from questions left join votes on questions.id=votes.entry_id and votes.entry_type='questions' group by questions.id])->hashes->to_array }
-sub unanswered { shift->pg->db->query(q[select questions.id,question,SUM(CASE WHEN vote = 'up' THEN 1 ELSE 0 END) as votes_up, SUM(CASE WHEN vote = 'down' THEN 1 ELSE 0 END) as votes_down from questions left join votes on questions.id=votes.entry_id and votes.entry_type='questions' group by questions.id])->hashes->to_array }
+sub all { shift->pg->db->query(q[
+  select
+    questions.id,
+    question,
+    questions.created,
+    questions.modified,
+    questions.username,
+    SUM(CASE WHEN vote = 'up' THEN 1 ELSE 0 END) as votes_up,
+    SUM(CASE WHEN vote = 'down' THEN 1 ELSE 0 END) as votes_down,
+    SUM(CASE WHEN vote = 'up' THEN 1 ELSE -1 END) as votes,
+    (
+      select comment
+      from comments
+      where answer is not null and question_id=questions.id
+    ) as answer,
+    (
+      select answered
+      from comments
+      where answer is not null and question_id=questions.id
+    ) as answered,
+    (
+      select answer
+      from comments
+      where answer is not null and question_id=questions.id
+    ) as answered_by
+  from questions
+    left join votes on questions.id=votes.entry_id and votes.entry_type='questions'
+    left join comments on questions.id=comments.question_id
+  group by questions.id
+  order by 9 desc,votes desc,questions.created
+])->hashes->to_array }
+sub answered { shift->pg->db->query(q[select questions.id,question,SUM(CASE WHEN vote = 'up' THEN 1 ELSE 0 END) as votes_up, SUM(CASE WHEN vote = 'down' THEN 1 ELSE 0 END) as votes_down from questions left join votes on questions.id=votes.entry_id and votes.entry_type='questions' left join comments on questions.id=comments.question_id where answer is not null group by questions.id])->hashes->to_array }
+sub unanswered { shift->pg->db->query(q[select questions.id,question,SUM(CASE WHEN vote = 'up' THEN 1 ELSE 0 END) as votes_up, SUM(CASE WHEN vote = 'down' THEN 1 ELSE 0 END) as votes_down from questions left join votes on questions.id=votes.entry_id and votes.entry_type='questions' left join comments on questions.id=comments.question_id where answer is null group by questions.id])->hashes->to_array }
 
 sub find { shift->pg->db->query('select * from questions where id = ?', shift)->hash }
 
