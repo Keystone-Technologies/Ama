3e5367b0a0f283617ccd96a41502c689064a7fea
Stefan Adams <stefan@borgia.com>
2015-06-03 14:31:47 -0500

    Add flagging

---
+++ lib/Ama/Controller/Flags.pm
@@ -0,0 +1,30 @@
+package Ama::Controller::Flags;
+use Mojo::Base 'Mojolicious::Controller';
+
+sub count {
+  my $self = shift;
+
+  my $entry_type = $self->param('entry_type');
+  my $entry_id = $self->param('entry_id');
+  $self->render(json => $self->flags->count($entry_type, $entry_id));
+}
+
+sub raise {
+  my $self = shift;
+
+  my $entry_type = $self->param('entry_type');
+  my $entry_id = $self->param('entry_id');
+  my $username = $self->session->{username};
+  $self->render(json => $self->flags->raise($entry_type, $entry_id, $username));
+}
+
+sub remove {
+  my $self = shift;
+
+  my $entry_type = $self->param('entry_type');
+  my $entry_id = $self->param('entry_id');
+  my $username = $self->session->{username};
+  $self->render(json => $self->flags->remove($entry_type, $entry_id, $username));
+}
+
+1;
---
+++ lib/Ama/Model/Flags.pm
@@ -0,0 +1,32 @@
+package Ama::Model::Flags;
+use Mojo::Base -base;
+
+has 'pg';
+
+sub count {
+  my ($self, $entry_type, $entry_id) = @_;
+  my $sql = 'select count(*) as flags from flags where entry_type = ? and entry_id = ?';
+  return $self->pg->db->query($sql, $entry_type, $entry_id)->hash;
+}
+
+sub raise {
+  my ($self, $entry_type, $entry_id, $username) = @_;
+  my $flags = $self->pg->db->query('select count(*) as flags from flags where entry_type = ? and entry_id = ? and username = ?', $entry_type, $entry_id, $username)->hash->{flags};
+  if ( !$flags ) {
+    my $sql = 'insert into flags (entry_type, entry_id, username) values (?, ?, ?)';
+    $self->pg->db->query($sql, $entry_type, $entry_id, $username);
+  }
+  $self->count($entry_type, $entry_id);
+}
+
+sub remove {
+  my ($self, $entry_type, $entry_id, $username) = @_;
+  my $flags = $self->pg->db->query('select count(*) as flags from flags where entry_type = ? and entry_id = ? and username = ?', $entry_type, $entry_id, $username)->hash->{flags};
+  if ( $flags ) {
+    my $sql = 'delete from flags where entry_type = ? and entry_id = ? and username = ?';
+    $self->pg->db->query($sql, $entry_type, $entry_id, $username);
+  }
+  $self->count($entry_type, $entry_id);
+}
+
+1;
