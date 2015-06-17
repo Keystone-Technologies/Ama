f05c35d0fe0cdb101fe4e16e15ab4bbb7df292a6
Jacob Dorpinghaus <jdorp@charter.net>
2015-06-17 19:56:12 +0000

    Prepping for 1st release

--- migrations/ama.sql
+++ migrations/ama.sql
@@ -30,8 +30,6 @@ insert into questions (question, username) values
   ('What is the second element of the periodic table of elements?', 'anonymous1'),
   ('What is the third element of the periodic table of elements?', 'anonymous2');
 
--- need a constraint that cannot edit or delete any comments but your own
--- need a constraint that cannot edit or delete comments of answered questions
 create table if not exists comments (
   comment_id  serial primary key,
   question_id int not null REFERENCES questions on delete cascade,
@@ -56,7 +54,6 @@ insert into answers (comment_id, question_id, username) values
   (3, 1, 'anonymous1'),
   (9, 3, 'anonymous2');
 
--- need a constraint that inserting a comment also inserts an up vote.
 create table if not exists votes (
   entry_type  text not null,
   entry_id    int not null,
@@ -82,7 +79,6 @@ insert into votes (entry_type, entry_id, vote, username) values
   ('comments', 11, 'up', 'anonymous1'), ('comments', 11, 'up', 'anonymous2'), ('comments', 11, 'down', 'anonymous3'), ('comments', 11, 'up', 'anonymous4'),
   ('comments', 12, 'down', 'anonymous1'), ('comments', 12, 'down', 'anonymous2'), ('comments', 12, 'down', 'anonymous3');
 
--- need a constraint of cannot flag your own comment
 create table if not exists flags (
   entry_type  text not null,
   entry_id    int not null,
@@ -94,11 +90,6 @@ insert into flags (entry_type, entry_id, username) values
   ('questions', 2, 'anonymous4'),
   ('comments', 5, 'anonymous3'),
   ('comments', 9, 'anonymous2');
-
--- questions index
--- select questions.id as question_id, question, votes('questions', questions.id) as votes, (select vote from votes where entry_type='questions' and entry_id=questions.id and username='anonymous2') as my_vote,flags('questions',questions.id) as flags, questions.username, questions.created, questions.modified, (case when comment is null then 0 else 1 end) as answered from questions left join answers on questions.id=answers.question_id left join comments on answers.comment_id=comments.id order by answered asc, votes desc, created desc;
--- comments index
--- select comments.id as comment_id,comments.question_id,comment,comments.username as username,comments.created,modified,votes('comments',comments.id) votes,(select vote from votes where entry_type='comments' and entry_id=comments.id and username='anonymous2') as my_vote,flagged('comments',comments.id) flagged, answers.username as approver,answers.created as approved from comments left join answers on comments.id=answers.comment_id where comments.question_id=1;
 
 CREATE OR REPLACE FUNCTION answered(integer) 
 RETURNS boolean AS
