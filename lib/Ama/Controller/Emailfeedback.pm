e2f5cece3c58491b8edc7afcd08d75d4ffeb2a05
andygoodday <xudongjin@yahoo.com>
2016-07-07 15:34:40 +0000

    replace sql

--- lib/Ama/Controller/Emailfeedback.pm
+++ lib/Ama/Controller/Emailfeedback.pm
@@ -7,15 +7,14 @@ sub submit{
     my $feedback = $c->param('feedback');
     #$c -> feedback -> submit($feedback);
     warn $c -> req -> body;
-    $sendgrid -> mail(
+    my $send = $sendgrid -> mail(
         text => $feedback,
         to      => q(ajin@keystone-it.com),
         #cc      => q(jsiems@keystone-it.com),
         #bcc     => q(bmeyer@keystone-it.com),
         from    => q(ajin@keystone-it.com),
-        subject => "feedback received from AMA",)->send->res->json,
-        #{message => 'success'},
-        $c -> render(json => {message => 'success'});
+        subject => "feedback received from AMA",)->send;
+    $c -> render(json => $send ->res ->json);
 }
  #$sendgrid->on(mail_send => sub {
     #my ($sendgrid, $ua, $tx) = @_;
