a922c6f3154d5c3b21b9f53a854301f262314bf3
Josh <joshuasiems@gmail.com>
2016-07-25 14:28:38 +0000

    Fixes issue #154

--- lib/Ama/Model/Questions.pm
+++ lib/Ama/Model/Questions.pm
@@ -73,7 +73,6 @@ sub getQuestions {
   'select '.
     'question_id, '.
     'question, '.
-    #"to_char(questions.created, 'MM/DD/YYYY HH12:MI:SS') as created, ".
     'created, ' .
     'username, '.
     '(select video_link from comments where comment_id = (select comment_id from answers where question_id = questions.question_id)) as video_link, ' .
