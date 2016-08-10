0387b7121e196892bdf1d6122042de3d7ee1ea67
andygoodday <xudongjin@yahoo.com>
2016-08-10 19:24:50 +0000

    When submitting feedback, check to and from address in conf file

--- lib/Ama/Controller/Feedback.pm
+++ lib/Ama/Controller/Feedback.pm
@@ -4,15 +4,20 @@ use Mojo::Base 'Mojolicious::Controller';
 sub submit{
   my $self = shift;
   my $sendgrid = $self->sendgrid;
+  my $send;
   my $feedback = $self->param('feedback');
-  my $send = $sendgrid->mail(
-               text    => $feedback,
-               to      => $self->config->{sendgrid}->{to},
-               cc      => $self->config->{sendgrid}->{cc},
-               bcc     => $self->config->{sendgrid}->{bcc},
-               from    => $self->config->{sendgrid}->{from},
-               subject => "feedback received from AMA",
-             )->send;
+  if (!$self->config->{sendgrid}->{to} || $self->config->{sendgrid}->{to} eq undef || !$self->config->{sendgrid}->{from} || $self->config->{sendgrid}->{from} eq undef){
+                warn "missing to or from address!\n";
+              } else{
+                    $send = $sendgrid->mail(
+                      text    => $feedback,
+                      to      => $self->config->{sendgrid}->{to},
+                      cc      => $self->config->{sendgrid}->{cc},
+                      bcc     => $self->config->{sendgrid}->{bcc},
+                      from    => $self->config->{sendgrid}->{from},
+                      subject => "feedback received from AMA",
+                    )->send;
+              }
   $self->feedback->add($feedback);
   $self->render(json => $send->res->json);
 }
